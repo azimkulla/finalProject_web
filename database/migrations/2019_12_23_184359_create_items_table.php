b2353002022d30987a53618d2694286581fa97e1
azimkulla <55102803+azimkulla@users.noreply.github.com>
2019-12-26 15:23:44 +0600

    Add files via upload

---
+++ database/migrations/2019_12_23_184359_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image_path');
+            $table->double('price');
+            $table->bigInteger('category_id')
+                ->unsigned();
+            $table->foreign('category_id')
+                ->on('categories')
+                ->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
